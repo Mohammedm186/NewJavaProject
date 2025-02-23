39fbdce6d7550959afc8fd3482dea1be69fd3040
dell <dell@DESKTOP-MGUN18Q>
2025-02-24 03:06:13 +0530

    second commit

--- src/test1.java
+++ src/test1.java
@@ -3,6 +3,9 @@ public class test1 {
 
 	public static void main(String[] args) {
 		System.out.println("hi....");
+		System.out.println("hi....");
+		System.out.println("hi....");
+		System.out.println("hi....");
 
 	}
 
